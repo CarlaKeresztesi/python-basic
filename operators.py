6fbc312a8dfce65c5880b0062edd2a29d8aefd2e
carla <carlakeresztesi@gmail.com>
2025-10-03 13:29:28 +0100

    Math Operators

--- operators.py
+++ operators.py
@@ -5,3 +5,12 @@ age = 20
 if age >= 17:
     print("You can start to drive, with a supervisor")
 # else... not able to start to drive...
+
+# Math operators: +, - * / %
+print(19 + 10.1)
+
+# Modulo operator finds the remainder after the division of numbers:
+print(13 % 3)
+
+# Operator precedence = Parenthesis Exponentials Division Mupltiplication Addition Subtraction
+print(10 * 4 / 2)
