9511926d5d7963ea2b203f8bf0c2a6e0bb0a62b7
carla <carlakeresztesi@gmail.com>
2025-10-22 20:04:09 +0100

    Added while loops and for loop with a dict to loops

--- loops.py
+++ loops.py
@@ -2,6 +2,8 @@
 #    - regularly used with collections(list and dictionaries) and alongside control flow(if)
 #    - in Python, we used 2 key types of loops -> while and -> for loops
 
+# for... loops - rely on iterables(- in Python that is a list, a dictionary or a string) to know how many iterations to complete
+
 shopping_list = ['eggs', 'milk', 'bread', 'coffee']
 
 for grocery in shopping_list:
@@ -16,7 +18,7 @@ for grocery in shopping_list:
 # --> : - this is the end of the for loop declaration
 # --> print(grocery) - this is where we use the named parameter
 
-# example with List made of numbers:
+# for... in with list made of numbers:
 def double_numbers(num_list):
     doubled = []
 
@@ -26,3 +28,25 @@ def double_numbers(num_list):
 
 numbers = [1, 2, 3, 4]
 print(double_numbers(numbers))
+
+# for... in with dict:
+customers = {
+    "name": "Ollie",
+    "eye-colour": "brown",
+    "age": 45,
+    "likes-chocolate": True
+}
+
+for customer in customers.values():
+    print(customer)
+
+# while loop - loop that runs whilst a condition is true
+
+# infinite loop example:
+# while True:
+#     print("Loop is running!")
+
+counter = 0
+while counter < 10:
+    print(counter)
+    counter += 1 # if i don't increment the counter it stays at 0 so it is still an infinite loop
