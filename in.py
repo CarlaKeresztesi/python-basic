cf37eda894ba1fa3fe1ed91e10f194409673989e
carla <carlakeresztesi@gmail.com>
2025-10-22 20:18:04 +0100

    Added 'in' keyword file with iteration and check value is present examples

---
+++ in.py
@@ -0,0 +1,19 @@
+#   in keyword:
+
+#   --> used to iterate through a sequence in a for loop;
+
+gift_list = ['boots', 'perfume', 'lippy', 'card']
+
+for gift in gift_list:
+    print('- ' + gift)
+
+#   -->used to check if a value is present in a sequence(list, range or string, etc.).
+
+people = ['James', 'Lucy', 'Wally', 'Sandeep']
+
+if 'Wally' in people:
+    print('We found Wally!')
+
+# scenario if 'Wally' is not in the list:
+# else:
+    #print('We did not find Wally!')
