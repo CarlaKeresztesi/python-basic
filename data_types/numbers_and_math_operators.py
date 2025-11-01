95d45918500c984f9324dba74aff9136a0b44975
carla <carlakeresztesi@gmail.com>
2025-11-01 14:16:03 +0000

    Added data_types, numbers and math op

---
+++ data_types/numbers_and_math_operators.py
@@ -0,0 +1,30 @@
+""""
+Numbers and math operators
+
+T-shirts - 5 (int)
+Cost per T-shirt - 5.50 (float)
+
+Operator |                   Meaning                    |        Example                |
+   +     |    Add                                       |      4 + 4 (8)                |
+   -     |    Subtract                                  |      4 - 2 (4)                |
+   *     |    Multiply                                  |      3 * 4 (12)               |
+   /     |    Divide                                    |      6 / 2 (3)                |
+   %     |    Modulo - (divisible by) returns remainder |      5 % 2 (remainder = 1)    |
+
+Work out total cost of our t-shirt order:
+"""""
+#print(type(5))
+#print(type(5.50))
+
+print(5 * 5.50)
+
+"""""
+Operator precedence works in the order of:
+    Parenthesis
+    Exponential
+    Division
+    Multiplication
+    Addition  
+    Subtraction 
+	a common acronym used is PEDMAS.
+"""""
